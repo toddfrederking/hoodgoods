3184396840a6af09fa03269d8b649d99904da993
TODD FREDERKING <toddfrederking@gmail.com>
2018-08-03 19:08:07 -0500

    todd is awesome

--- src/server/Routes/tshirtroutes.js
+++ src/server/Routes/tshirtroutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const {list,show,checkout } = require( "../controllers/tshirtController");
+const { list, show, checkout } = require( "../Controllers/tshirtcontroller");
 
 
 router.get("/tshirts", list);
